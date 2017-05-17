1ec6fff4387bd18150f959783b4affefa88758fc
Sanad Liaquat <sanadhussain@gmail.com>
2017-05-17 11:53:44 +0500

    Initial commit

---
+++ src/components/posts_new.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {createPost} from '../actions';
+
+
+class PostsNew extends Component {
+
+
+    renderField(field){
+
+        const {meta: {touched, error} } = field;
+        const className = `form-group ${touched && error ? 'has-danger' : ''}`
+
+        return(
+            <div className={className}>
+                <label>{field.label}</label>
+                <input className="form-control" type="text" {...field.input}/>
+                <div className="text-help">
+                    {touched ? error : ''}
+                </div>
+            </div>
+        );
+    }
+
+    onSubmit(values){
+        this.props.createPost(values, ()=> {
+            this.props.history.push('/');
+        });
+    }
+
+    render(){
+
+        const { handleSubmit } = this.props;
+
+        return (
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                <Field name="title" label="Title" component={this.renderField}/>
+                <Field name="categories" label="Categories" component={this.renderField}/>
+                <Field name="content" label="Post content" component={this.renderField}/>
+                <button type="submit" className="btn btn-primary">Submit</button>
+                <Link to="/" className="btn btn-danger">Cancel</Link>
+            </form>
+        )
+    }
+}
+
+function validate(values){
+    const errors ={};
+
+    if(!values.title)
+        errors.title = 'Enter a title'
+    if(!values.categories)
+        errors.categories = 'Enter a category'
+    if(!values.content)
+        errors.content = 'Enter some content'
+
+    // If errors is empty, the form is fine to submit
+    // If errors has any properties, redux from assumes form is invalid
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'PostsNewForm'
+})(
+    connect(null, {createPost})(PostsNew)
+);
